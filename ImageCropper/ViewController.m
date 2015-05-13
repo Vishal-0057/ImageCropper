907c5edeb792595c3ebaa0c5f2f16ebf5681d318
VISHAL GUPTA <vishal.gupta@intelligrape.com>
2015-05-13 19:33:31 +0530

    Initial Commit

---
+++ ImageCropper/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ImageCropper
+//
+//  Created by abhayam rastogi on 5/13/15.
+//  Copyright (c) 2015 Itelligrape. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
